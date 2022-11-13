1da1a2c91bf2a81c233e29c67b7729795bdcb854
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2022-11-12 21:17:27 -0300

    feat: :sparkles: add eliminating duplications

---
+++ selects-basicos/eliminando-duplicacoes.sql
@@ -0,0 +1,11 @@
+SELECT nome 
+FROM aula
+WHERE nome = 'Conclusão do Módulo';
+  
+SELECT DISTINCT nome 
+FROM aula
+WHERE nome = 'Conclusão do Módulo';
+
+SELECT DISTINCT nome, duracao_segundos
+FROM aula
+WHERE nome = 'Conclusão do Módulo';
